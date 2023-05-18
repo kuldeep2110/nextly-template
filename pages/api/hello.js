871e6e700a8942a133592c8b365930527d4ec21f
Kuldeep <kuldeep.home.mail@gmail.com>
2023-05-18 14:28:29 +0530

    pages-updated

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const helloAPI = (req, res) =>{
+export default function helloAPI(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
-
-export default helloAPI;
