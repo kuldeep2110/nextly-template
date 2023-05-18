50d3b8992bf945da7c01c76e997e6a082818db41
Kuldeep <kuldeep.home.mail@gmail.com>
2023-05-18 14:55:36 +0530

    icons

--- components/footer.js
+++ components/footer.js
@@ -2,34 +2,34 @@ import Link from "next/link";
 import React from "react";
 import Container from "./container";
 
-import {
-  AiOutlineTwitter
-} from 'react-icons/ai'
-import {
-  FaTiktok, FaTelegramPlane
-} from 'react-icons/fa'
+// import {
+//   AiOutlineTwitter
+// } from 'react-icons/ai'
+// import {
+//   FaTiktok, FaTelegramPlane
+// } from 'react-icons/fa'
 
 export default function Footer() {
   const navigation = [
     {
-      name : "Home",
-      link:"./"
-    }, 
+      name: "Home",
+      link: "./"
+    },
     {
-      name:"Revenue Model",
-      link:"#model"
-    }, 
+      name: "Revenue Model",
+      link: "#model"
+    },
     {
       name: "Tokenomics",
       link: "#tokenomics"
-    }, 
+    },
     {
       name: "Roadmap",
-      link:"#roadmap"
-    }, 
+      link: "#roadmap"
+    },
     {
-      name : "FAQ",
-      link:"#faq"
+      name: "FAQ",
+      link: "#faq"
     }
   ];
   const legal = ["Terms", "Privacy", "Legal"];
@@ -56,7 +56,7 @@ export default function Footer() {
             </div>
 
             <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
-             FOMO is the backbone of our market ready decentralized advertising platform. Meet the utility token powering our ecosystem.
+              FOMO is the backbone of our market ready decentralized advertising platform. Meet the utility token powering our ecosystem.
             </div>
           </div>
 
@@ -90,21 +90,24 @@ export default function Footer() {
                 target="_blank"
                 rel="noopener">
                 <span className="sr-only">Twitter</span>
-                <AiOutlineTwitter className="iconFooter" />
+                {/* <AiOutlineTwitter className="iconFooter" /> */}
+                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="iconFooter" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0 0 75-94 336.64 336.64 0 0 1-108.2 41.2A170.1 170.1 0 0 0 672 174c-94.5 0-170.5 76.6-170.5 170.6 0 13.2 1.6 26.4 4.2 39.1-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 0 0-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 0 1-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 0 1-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850 671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2 33.2-24.3 62.3-54.4 85.5-88.2z"></path></svg>
               </a>
               <a
                 href="https://t.me/DailyFOMOToken"
                 target="_blank"
                 rel="noopener">
                 <span className="sr-only">Telegram</span>
-                <FaTelegramPlane  className="iconFooter"/>
+                {/* <FaTelegramPlane className="iconFooter" /> */}
+                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" className="iconFooter" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z"></path></svg>
               </a>
               <a
                 href="https://www.tiktok.com/@thedailyfomo"
                 target="_blank"
                 rel="noopener">
                 <span className="sr-only">tiktok</span>
-                <FaTiktok  className="iconFooter" />
+                {/* <FaTiktok className="iconFooter" /> */}
+                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" className="iconFooter" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"></path></svg>
               </a>
               <a href="https://www.dailyfomo.net" target="_blank" rel="noopener">
                 <span className="sr-only">Dailyfomo</span>
@@ -115,7 +118,7 @@ export default function Footer() {
         </div>
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. 
+          Copyright © {new Date().getFullYear()}.
           Made with by Daily FOMO. Development by   Web3Templates.
           {/*<a href="https://web3templates.com/" target="_blank" rel="noopener">
             Web3Templates.
